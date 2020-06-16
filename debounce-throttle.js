92d6dd38c560ee5605a05bc60cb21457f48efa7f
Lux <luxliu007168@gmail.com>
2020-06-16 20:21:28 +1200

    Added debounce and throttle

---
+++ debounce-throttle.js
@@ -0,0 +1,29 @@
+const throttle = (fn, delay = 100) => {
+  let timer = null;
+
+  return function () {
+    if (timer) {
+      return;
+    }
+
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+      timer = null;
+    }, delay);
+  };
+};
+
+const debounce = (fn, delay = 100) => {
+  let timer = null;
+
+  return function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+      timer = null;
+    }, delay);
+  };
+};
